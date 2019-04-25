4c24420e7d8480803a778255b521ee93bc02c800
liuy <liuy@pzdf.com>
2019-04-25 19:31:53 +0800

    添加okhttp代码

---
+++ app/src/main/java/com/android/myframework/retrofit/architect_day33/simple2/Result.java
@@ -0,0 +1,9 @@
+package com.android.myframework.retrofit.architect_day33.simple2;
+
+/**
+ * Created by hcDarren on 2017/12/16.
+ */
+
+public class Result<T> extends BaseResult{
+    public Object data;
+}
---
+++ app/src/main/java/com/android/myframework/retrofit/architect_day34/retrofit/Call.java
@@ -0,0 +1,9 @@
+package com.android.myframework.retrofit.architect_day34.retrofit;
+
+/**
+ * Created by hcDarren on 2017/12/17.
+ */
+
+public interface Call<T> {
+    void enqueue(Callback<T> callback);
+}
